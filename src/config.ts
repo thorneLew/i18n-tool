02bea8aff73dd2da5016a2506d2b5410dad3e688
thonre <thorne_lew@126.com>
2021-08-24 16:03:25 +0800

    fix: config.color 取之错误

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export function getConfig() {
 	const rootPath: string | undefined = config.get('i18n-tools.rootPath'); // 语言包路径
 	const regText: {pattern: string, modifiers: string} | undefined = config.get('i18n-tools.regText'); // 正则
 	const sourceLanguage: string | undefined = config.get('i18n-tools.sourceLanguage'); // 当前语言
-	const color: string | undefined = config.get('i18n-tools.sourceLanguage') || '#d6a01d'; // 当前语言
+	const color: string | undefined = config.get('i18n-tools.color') || '#d6a01d'; // 当前语言
   
 	return {
 		rootPath: rootPath,
